9026c5a6d3e89c6d0320585f7e6bf78cc14d7847
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2025-06-22 13:47:37 +0000

    styling config bug fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,3 @@
-// module.exports = {
-//   plugins: [
-//     require('@tailwindcss/postcss7-compat'), // Tailwind CSS plugin for PostCSS 7
-//     require('autoprefixer'),
-//   ],
-// }
-
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,4 +23,4 @@ module.exports = {
   },
   plugins: [],
 }
-tailwind.config.js
+
